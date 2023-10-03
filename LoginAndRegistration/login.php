d6c7df1d51ce410d6a4268603fea700a54ff26d0
roldsujej <59650518+roldsujej@users.noreply.github.com>
2023-10-03 20:23:51 +0800

    fixed the db config

--- LoginAndRegistration/login.php
+++ LoginAndRegistration/login.php
@@ -1,47 +1,87 @@
 <?php
-require_once '../database/config.php';
-session_start();
-if (isset($_SESSION['user'])) {
-    header('location: Applicant/applicantDashboard.php');
-}
-if (isset($_SESSION['admin'])) {
-    header('location: ADMIN/adminDashboard.php');
-}
-if (isset($_POST['login'])) {
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-    // Check user login
-    $userStmt = $conn->prepare("SELECT * FROM account_profiles WHERE email = ?");
-    $userStmt->bind_param("s", $email);
-    $userStmt->execute();
-    $userResult = $userStmt->get_result();
+session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once '../database/config.php';
+include 'common.php';
+
+
+
+if (isset($_POST['login'])) {
+    $email = mysqli_real_escape_string($conn, $_POST["email"]);
+    $password = mysqli_real_escape_string($conn, $_POST["password"]);
+
+    // Check member login
+    $memberQuery = $conn->prepare("SELECT * FROM account_profiles WHERE email = ?");
+    $memberQuery->bind_param("s", $email);
+    $memberQuery->execute();
+    $memberResult = $memberQuery->get_result();
 
     // Check admin login
-    $adminStmt = $conn->prepare("SELECT * FROM tbl_admin WHERE email = ?");
-    $adminStmt->bind_param("s", $email);
-    $adminStmt->execute();
-    $adminResult = $adminStmt->get_result();
+    $adminQuery = $conn->prepare("SELECT * FROM tbl_admin WHERE email = ?");
+    $adminQuery->bind_param("s", $email);
+    $adminQuery->execute();
+    $adminResult = $adminQuery->get_result();
 
-    if ($userResult->num_rows > 0) {
-        $row = $userResult->fetch_assoc();
+    if ($memberResult->num_rows > 0) {
+        $row = $memberResult->fetch_assoc();
+
         if (password_verify($password, $row['password'])) {
-            if ($row['status'] == 1 && $row['user_role'] == 0) {
-                $_SESSION['user'] = $row;
-                header("location: application.php");
+
+            if ($row['status'] == 0 && $row['user_role'] == 0) {    // di verified email
+                // Email not verified
+                $_SESSION['login_error'] = "Email not verified. Please check your email for verification instructions.";
+                header("location: notverified.php");
                 exit();
-            } elseif ($row['status'] == 1 && $row['user_role'] == 1) {
-                $_SESSION['user'] = $row;
-                header("location: MEMBER/memberDashboard.html");
+            } elseif ($row['status'] == 1 && $row['user_role'] == 0) {
+                //  Email applicant Member login
+
+                //$_SESSION['user'] = $row;
+                // $_SESSION['fullName'] = $row['firstName'] . "" .  $row['lastName'];
+                //  $_SESSION['applicant_id'] = $row['id'];
+                $_SESSION['fullName'] = $row['firstName'] . ' ' . $row['lastName'];
+                // $_SESSION['fname'] = $row['firstName'];
+                // $_SESSION['lname'] = $row['lastName'];
+                $_SESSION['id'] = $row['id'];
+                //$_SESSION['status'] = $row['status'];
+                $_SESSION['user_role'] =  'applicant';
+                $_SESSION['email'] = $row['email'];
+
+                //echo $_SESSION['fullname'];
+                header("location: ../Applicant/applicantDashboard.php");
                 exit();
             }
         } else {
-            echo '<script>alert("Incorrect email or password!");window.location.href = "login.php";</script>';
+            $_SESSION['login_error'] = "Incorrect email or password.";
+            header("location: login.php");
+            exit();
         }
     } elseif ($adminResult->num_rows > 0) {
         $row = $adminResult->fetch_assoc();
+        if ($_POST['$password'] === $row['password']) {
+            if ($row['account_status'] == 0) {
+                // Email not verified for admin
+                $_SESSION['login_error'] = "Email not verified for admin. Please check your email for verification instructions.";
+                header("location: login.php");
+                exit();
+            } elseif ($row['membership_status'] == 'admin') {
+                // Admin login
+                $_SESSION['user_id'] = $row['id'];
+                $_SESSION['user_role'] = 'admin';
+                $_SESSION['email'] = $row['email'];
+                header("location: capeda/ADMIN/admindashboard.php");
+                exit();
+            }
+        } else {
+            $_SESSION['login_error'] = "Incorrect email or password.";
+            header("location: login.php");
+            exit();
+        }
     } else {
-        echo '<script>alert("Account does not exist!");window.location.href = "login.php";</script>';
+        $_SESSION['login_error'] = "Account not found.";
+        header("location: login.php");
+        exit();
     }
 }
 ?>
@@ -52,7 +92,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="icon" href="../images/logo.jpg" type="image/x-icon">
+    <link rel="icon" href="../images/capedalogo.png" type="image/x-icon">
     <title>CAPEDA | Login</title>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
     <link rel="stylesheet" href="../assets/loginAndRegistration/css/login.css" />
@@ -73,7 +113,7 @@ if (isset($_POST['login'])) {
                 <div class="loginBody">
                     <div class="text-input">
                         <i class="ri-user-fill"></i>
-                        <input type="text" name="username" placeholder="Username" />
+                        <input type="text" name="email" placeholder="Username" />
                     </div>
                     <div class="text-input">
                         <i class="ri-lock-fill"></i>
--- database/config.php
+++ database/config.php
@@ -1,8 +1,7 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","capeda");
+$conn = mysqli_connect("localhost", "root", "", "capeda2");
 
-if(!$conn){
-    echo "Connection error".mysqli_connect_error()or die;
-
+if (!$conn) {
+    echo "Connection error" . mysqli_connect_error() or die;
 }
