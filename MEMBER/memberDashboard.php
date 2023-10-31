001aa00a2748e94e7f26086d1312b4fd834f31a3
shalaire91 <shalaire91@gmail.com>
2023-11-01 04:17:34 +0800

    forgot to change directory

--- MEMBER/memberDashboard.php
+++ MEMBER/memberDashboard.php
@@ -188,7 +188,7 @@ require "../database/config.php";
 
 
     <!---------------SCRIPT--------------------------->
-    <script src="../script/MEMBER/memberDashboard.js"></script>
+    <script src="../js/MEMBER/memberDashboard.js"></script>
     <!---------ICONS----------------------------------->
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
